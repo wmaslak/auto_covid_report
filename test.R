b6994f42df470597e6255e661b1d80193211939a
Wojciech Maślakiewicz <wmaslakiewicz@gmail.com>
2021-05-14 18:25:04 +0200

    testing

--- test.R
+++ test.R
@@ -1,4 +1,11 @@
 install.packages("COVID19")
+install.packages('wbstats')
 
 # load the package
 library("COVID19")
+library("wbstats")
+
+gmr <- "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
+x<- covid19(country = 'Poland', 
+            wb = c("gdp_per_capita" = "NY.GDP.PCAP.PP.CD" ),
+            gmr = gmr)
